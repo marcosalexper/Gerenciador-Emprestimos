35490034e172b914f8a531a0af89473050db19bf
marcosalexper <marcosalexper@hotmail.com>
2024-09-03 20:51:45 -0300

    feat: atualizado classes modelo

--- GerenciadorEmprestimos/src/main/java/modelo/Cliente.java
+++ GerenciadorEmprestimos/src/main/java/modelo/Cliente.java
@@ -1,7 +1,61 @@
-
 package modelo;
 
-
 public class Cliente {
-    
+
+    private int id;
+    private String nome;
+    private String telefone;
+    private int totalEmprestimos;
+
+    public Cliente() {
+    }
+
+    public Cliente(String nome, String telefone) {
+        this.nome = nome;
+        this.telefone = telefone;
+    }
+
+    public Cliente(int id, String nome, String telefone) {
+        this.id = id;
+        this.nome = nome;
+        this.telefone = telefone;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public int getTotalEmprestimos() {
+        return totalEmprestimos;
+    }
+
+    public void setTotalEmprestimos(int totalEmprestimos) {
+        this.totalEmprestimos = totalEmprestimos;
+    }
+
+    @Override
+    public String toString() {
+        return "id: " + id + ", nome: " + nome;
+    }
+
 }
--- GerenciadorEmprestimos/src/main/java/modelo/Emprestimo.java
+++ GerenciadorEmprestimos/src/main/java/modelo/Emprestimo.java
@@ -1,6 +1,72 @@
-
 package modelo;
 
+import java.sql.Date;
+
 public class Emprestimo {
-    
+
+    private int id;
+    private Cliente idCliente;
+    private Ferramenta idFerramenta;
+    private Date dataEmprestimo;
+    private Date dataDevolucao;
+
+    public Emprestimo() {
+
+    }
+
+    public Emprestimo(Cliente idCliente, Ferramenta idFerramenta, Date dataEmprestimo, Date dataDevolucao) {
+        this.idCliente = idCliente;
+        this.idFerramenta = idFerramenta;
+        this.dataEmprestimo = dataEmprestimo;
+        this.dataDevolucao = dataDevolucao;
+    }
+
+    public Emprestimo(int id, Cliente idCliente, Ferramenta idFerramenta, Date dataEmprestimo, Date dataDevolucao) {
+        this.id = id;
+        this.idCliente = idCliente;
+        this.idFerramenta = idFerramenta;
+        this.dataEmprestimo = dataEmprestimo;
+        this.dataDevolucao = dataDevolucao;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Cliente getIdCliente() {
+        return idCliente;
+    }
+
+    public void setIdCliente(Cliente idCliente) {
+        this.idCliente = idCliente;
+    }
+
+    public Ferramenta getIdFerramenta() {
+        return idFerramenta;
+    }
+
+    public void setIdFerramenta(Ferramenta idFerramenta) {
+        this.idFerramenta = idFerramenta;
+    }
+
+    public Date getDataEmprestimo() {
+        return dataEmprestimo;
+    }
+
+    public void setDataEmprestimo(Date dataEmprestimo) {
+        this.dataEmprestimo = dataEmprestimo;
+    }
+
+    public Date getDataDevolucao() {
+        return dataDevolucao;
+    }
+
+    public void setDataDevolucao(Date dataDevolucao) {
+        this.dataDevolucao = dataDevolucao;
+    }
+
 }
