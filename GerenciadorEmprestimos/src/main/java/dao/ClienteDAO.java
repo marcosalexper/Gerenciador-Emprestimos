a1e1c14be78f7b63eb33532ab109c38fb18ba4d7
marcosalexper <marcosalexper@hotmail.com>
2024-09-06 15:43:44 -0300

    feat: criada classe conexao

--- GerenciadorEmprestimos/src/main/java/dao/ClienteDAO.java
+++ GerenciadorEmprestimos/src/main/java/dao/ClienteDAO.java
@@ -3,6 +3,11 @@ package dao;
 
 import modelo.Cliente;
 import java.util.ArrayList;
+import java.sql.Statement;
+import java.sql.SQLException;
+import java.sql.ResultSet;
+import java.sql.PreparedStatement;
+import java.sql.Connection;
 
 
 public class ClienteDAO {
@@ -15,8 +20,83 @@ public class ClienteDAO {
         minhaLista.clear();
         
         try {
-            Statement stmt = 
+            Statement stmt = this.getConexao().createStatement();
+            ResultSet res = stmt.executeQuery("SELECT * FROM tb_clientes");
+            while (res.next()) {
+
+                int id = res.getInt("id");
+                String nome = res.getString("nome");
+                String telefone = res.getString("telefone");
+
+                Cliente objeto = new Cliente(id, nome, telefone);
+
+                minhaLista.add(objeto);
+            }
+
+            stmt.close();
+
+        } catch (SQLException ex) {
+        }
+
+        return minhaLista;
+    }
+
+    public boolean insertClienteBD(Cliente objeto) {
+        String sql = "INSERT INTO tb_clientes(id,nome,telefone) VALUES(?,?,?)";
+
+        try {
+            PreparedStatement stmt = this.getConexao().prepareStatement(sql);
+
+            stmt.setInt(1, objeto.getId());
+            stmt.setString(2, objeto.getNome());
+            stmt.setString(3, objeto.getTelefone());
+
+            stmt.execute();
+            stmt.close();
+
+            return true;
+
+        } catch (SQLException erro) {
+            throw new RuntimeException(erro);
         }
     }
+
+   
+    public boolean deleteClienteBD(int id) {
+        try {
+            Statement stmt = this.getConexao().createStatement();
+            stmt.executeUpdate("DELETE FROM tb_clientes WHERE id = " + id);
+            stmt.close();
+
+        } catch (SQLException erro) {
+        }
+
+        return true;
+    }
+
+  
+    public boolean updateClienteBD(Cliente objeto) {
+
+        String sql = "UPDATE tb_clientes set nome = ? ,telefone = ? WHERE id = ?";
+
+        try {
+            PreparedStatement stmt = this.getConexao().prepareStatement(sql);
+
+            stmt.setString(1, objeto.getNome());
+            stmt.setString(2, objeto.getTelefone());
+            stmt.setInt(3, objeto.getId());
+
+            stmt.execute();
+            stmt.close();
+
+            return true;
+
+        } catch (SQLException erro) {
+            throw new RuntimeException(erro);
+        }
+
+    }
+        }
+    
     
 }
--- GerenciadorEmprestimos/src/main/java/dao/Conexao.java
+++ GerenciadorEmprestimos/src/main/java/dao/Conexao.java
@@ -1,13 +1,46 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package dao;
 
-/**
- *
- * @author Win11
- */
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Conexao {
+
     
+    public static Connection getConexao() {
+
+        Connection connection = null;
+
+        try {
+           
+            String driver = "com.mysql.cj.jdbc.Driver";
+            Class.forName(driver);
+
+            String server = "localhost";
+            String database = "gerenciadoremprestimos";
+            String url = "jdbc:mysql://" + server + ":3306/" + database;
+            String user = "root";
+            String password = "1234";
+
+            connection = DriverManager.getConnection(url, user, password);
+
+            
+            if (connection != null) {
+                System.out.println("Status: Conectado!");
+            } else {
+                System.out.println("Status: NAO CONECTADO!");
+            }
+
+            return connection;
+
+        } catch (ClassNotFoundException e) {  //Driver nao encontrado
+            System.out.println("O driver nao foi encontrado. " + e.getMessage());
+            return null;
+
+        } catch (SQLException e) {
+            System.out.println("Nao foi possivel conectar...");
+            return null;
+        }
+
+    }
 }
